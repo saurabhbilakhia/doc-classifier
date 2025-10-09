2da77d3194a294373994ec96d996cc8b9f902f5b
saurabhbilakhia <saurabh.bilakhia@gmail.com>
2025-10-09 00:27:39 -0400

    updated server url

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'http://191.101.0.217:8080',
         changeOrigin: true,
         secure: false,
       }
